ad3a002b4b16d0bc3bc4b57775d9817d66318b30
Kayla200212 <99227847+Kayla200212@users.noreply.github.com>
2023-11-16 05:16:42 +0000

    welp

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 10## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**__kayla willis_______________________
+**Name:**__cameron mclaren_______________________
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
@@ -9,9 +9,10 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
-
+The work is O(n+m) because it is a breadth first search and the worst case would be if each node and edge is visited once (rather than being able to reach nodes more easily with less edges)
 - **4)**
-
+The worst case would depend on the number of connected nodes in the graphs, with the worst case being the number of connected nodes, so n.
 - **5)**
-
+This also has a O(n+m) work because it entirely depends on the input from the reachable function's performance. The steps in connected are all at constant time so its work would be that of reachable, which it uses in its computations.
 - **7)**
+The work would change to O(n^2) because this changes how nodes are stored. It is represented as a matrix, Vertices x Vertices rather than just the addition of the vertex and nodes. 
--- main.py
+++ main.py
@@ -16,18 +16,24 @@ def reachable(graph, start_node):
     """
     result = set([start_node])
     frontier = set([start_node])
-    while len(frontier) != 0:
+    while len(frontier) != 0:#need this condition bc when frontier is empty, we're done
         ###TODO
-        pass
+         node = frontier.pop()#remove it from frontier, its our new node
+         result.add(node)#this node is part of the final solution in order
+         for next in graph[node]:
+           if next not in result:#i love python. if the next node isnt alr in result
+             frontier.add(next)#add it to the frontier
     return result
-
-
 
 
 
 def connected(graph):
     ### TODO
-    pass
+    numnodes = len(graph)#num of nodes is the length of graph
+    initnode = list(graph.keys()[0])#start node is key 0 on graph
+    reach = reachable(graph, initnode)#call reachable
+    return len(reach) == numnodes#this is a t/f check.
+    #return true if the len of reach is the same as num of nodes
 
 
 
@@ -38,5 +44,13 @@ def n_components(graph):
       the number of connected components in an undirected graph
     """
     ### TODO
-    pass
+    result = []
+    next = set(graph.keys())#this changes all of our nodes into a set to be parsed
+    while len(next) > 0:#continue while there are nodes left
+      done = next.pop()#remove
+      reach = reachable(graph,done)
+      next -= reach#both are sets
+      result.append(reach)#add on the node reached
+    return len(result)#keep going till out of notes and we just want the #
+      
 
